50828718489756b6a15a627aea174a3c14cfd7ce
Santiago Bandiera <sagaban@hotmail.com>
2019-05-07 11:32:19 -0300

    Create a list that user populate

--- App.js
+++ App.js
@@ -7,23 +7,67 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+// const instructions = Platform.select({
+//   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+//   android:
+//     'Double tap R on your keyboard to reload,\n' +
+//     'Shake or press menu button for dev menu',
+// });
 
 type Props = {};
-export default class App extends Component<Props> {
+type State = {
+  placeName: string,
+  places: Array<string>,
+}
+export default class App extends Component<Props, State> {
+  state = {
+    placeName: '',
+    places: []
+  }
+
+  // whit this syntax, `this` will refer the class
+  placeNameChangeHandler = (text: string) => {
+    this.setState({placeName: text});
+  }
+
+  placeSubmitHandler = () => {
+    if (this.state.placeName.trim() === '') {
+      return;
+    }
+
+    this.setState( prevState => {
+      return {
+        places: prevState.places.concat(prevState.placeName),
+        placeName: '',
+      }
+    })
+  }
+
   render() {
+    const placesOutput = this.state.places.map(
+      (place, i) => (
+        <Text key={place+i}>{place}</Text>
+      )
+    )
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.placeInput}
+            placeholder="Enter a place name"
+            onChangeText={this.placeNameChangeHandler}
+            value={this.state.placeName}
+          />
+          <Button
+            title="  Add  "
+            onPress={this.placeSubmitHandler}
+          />
+        </View>
+        <View>
+          {placesOutput}
+        </View>
       </View>
     );
   }
@@ -32,18 +76,23 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    padding: 20,
+    justifyContent: 'flex-start',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#FFE4C4',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  inputContainer: {
+    // flex: 1,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 20,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  placeInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    width: '70%',
   },
 });
