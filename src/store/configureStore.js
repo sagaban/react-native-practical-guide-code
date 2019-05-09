82855b410ca2878faa67cfd6e033f930b3b5eefb
Santiago Bandiera <sagaban@hotmail.com>
2019-05-09 13:03:07 -0300

    Enable Redux debugging

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,15 +1,21 @@
 /**
  * @format
  */
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose } from 'redux';
 import placesReducer from '@/store/reducers/places';
 
 const rootReducer = combineReducers({
   places: placesReducer
 });
 
+let composeEnhancer = compose;
+
+if (__DEV__){
+  composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+}
+
 const configureStore = () => {
-  return createStore(rootReducer);
+  return createStore(rootReducer, composeEnhancer());
 }
 
 export default configureStore;
