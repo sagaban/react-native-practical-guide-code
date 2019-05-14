c97172420bb1739fea121eb3eb1c5c6cd93b0a96
Santiago Bandiera <sagaban@hotmail.com>
2019-05-14 10:35:17 -0300

    Add sidemenu content

--- src/screens/SideDrawer/SideDrawer.js
+++ src/screens/SideDrawer/SideDrawer.js
@@ -3,14 +3,20 @@
  * @flow
  */
 import React, { Component } from 'react';
-import { Text, View, Dimensions, StyleSheet } from 'react-native';
+import { Text, View, Dimensions, StyleSheet, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 type Props = {};
 class SideDrawer extends Component<Props> {
   render() {
     return (
       <View style={[styles.container, { width: Dimensions.get('window').width * 0.8 }]}>
-        <Text> SideDrawer </Text>
+        <TouchableOpacity>
+          <View style={styles.drawerItem}>
+            <Icon name="ios-log-out" size={30} color="#aaa" style={styles.drawerItemIcon} />
+            <Text> Sign Out </Text>
+          </View>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -18,9 +24,18 @@ class SideDrawer extends Component<Props> {
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop: 22,
+    paddingTop: 50,
     backgroundColor: 'white',
     flex: 1,
+  },
+  drawerItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+    backgroundColor: '#eee',
+  },
+  drawerItemIcon: {
+    marginRight: 10,
   },
 });
 export default SideDrawer;
